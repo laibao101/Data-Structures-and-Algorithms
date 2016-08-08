8b2ec9f2c3af7731b8f6cc8252f40f7c0305952c
ivan zhou <ivanzhou@Andy-Mac-mini.local>
2016-08-08 09:46:22 +0800

    first initthe stack and add some methods and attributes

---
+++ stack.js
@@ -0,0 +1,40 @@
+
+
+/*
+	a simple stack with some method and some atrribute
+	1.stack init        	initialize a stack.
+	2.push      			push some elements into the stack.Meanwhile,the top pointer increases.
+	3.peek					return the element the top pointer pointed. 
+	4.pop					return the element the top pointer pointed,the top pointer decreases.
+	5.clear					get the stack clear and put the top pointer top 0.
+	6.length				return the total number of elements in the stack.
+*/ 
+
+
+
+
+function Stack() {
+        this.dataStore = [];
+        this.top = 0;
+        this.push = push;
+        this.pop = pop;
+        this.peek = peek;
+        this.clear = clear;
+        this.length = length;
+}
+function push(element) {
+        this.dataStore[this.top++] = element;
+}
+function peek() {
+        return this.dataStore[this.top-1];
+   
+}
+function pop() {
+        return this.dataStore[--this.top];
+}
+function clear() {
+        this.top = 0;
+}
+function length() {
+        return this.top;
+}
