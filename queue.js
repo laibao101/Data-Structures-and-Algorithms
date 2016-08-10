ff3157f7b889cf10fff202419253b473da209b48
ivan zhou <ivanzhou@Andy-Mac-mini.local>
2016-08-10 16:53:36 +0800

    test simple sort, radix sorting

--- queue.js
+++ queue.js
@@ -65,7 +65,7 @@ function print(item,...values){
 */
 
 
-const q=new Queue();
+// const q=new Queue();
 
 // q.enqueue("Meredith");
 // q.enqueue("Cynthia");
@@ -85,58 +85,104 @@ const q=new Queue();
 */
 
 
-function Dancer(name,sex){
-	this.name=name;
-	this.sex=sex;
-}
+// function Dancer(name,sex){
+// 	this.name=name;
+// 	this.sex=sex;
+// }
 
 
 
-function getDancers(males,females){
-	const names=read("partner.txt");
-	for(let item of names){
-		item=item.trim();
-	}
-	for(const item of names){
-		const dancer=item.split(" ");
+// function getDancers(males,females){
+// 	const names=read("partner.txt");
+// 	for(let item of names){
+// 		item=item.trim();
+// 	}
+// 	for(const item of names){
+// 		const dancer=item.split(" ");
 		
-		const sex=dancer[0];
-		const name=dancer[1];
-		if(sex=="F"){
-			females.enqueue(new Dancer(name,sex) );
+// 		const sex=dancer[0];
+// 		const name=dancer[1];
+// 		if(sex=="F"){
+// 			females.enqueue(new Dancer(name,sex) );
+// 		}else{
+// 			males.enqueue(new Dancer(name,sex) );
+// 		}
+// 	}
+
+// }
+
+
+// function dance(males,females){
+// 	print("The dancer partner are : \n");
+// 	while(!females.empty() && !males.empty() ){
+// 		const female=females.dequeue();
+// 		print("Female dancer is : " + female.name);
+// 		const male=males.dequeue();
+// 		print("and the male dancer is : " + male.name);
+// 	}
+// }
+
+
+// const males = new Queue();
+// const females = new Queue();
+// getDancers(males, females);
+// dance(males, females);
+// if (!females.empty()) {
+//     print(females.front().name + " is waiting to dance.");
+//  }
+//  if (!males.empty()) {
+//     print(males.front().name + " is waiting to dance.");
+//  }
+
+
+
+/*
+	2.use queue to sort the datas  --------  radix sorting
+	a simple sorting , sort number <100
+*/
+
+
+function distribute(nums,queues,n,digit){
+	for(const item of nums){
+		if(digit==1){
+			queues[item%10].enqueue(item);
 		}else{
-			males.enqueue(new Dancer(name,sex) );
+			queues[Math.floor(item/10)].enqueue(item);
 		}
 	}
-
 }
 
+function collect(queues,nums){
+	let i=0;
+	for (var digit = 0; digit < 10; ++digit) {
+       while (!queues[digit].empty()) {
+           nums[i++] = queues[digit].dequeue();
+       }
+   	}
+}
 
-function dance(males,females){
-	print("The dancer partner are : \n");
-	while(!females.empty() && !males.empty() ){
-		const female=females.dequeue();
-		print("Female dancer is : " + female.name);
-		const male=males.dequeue();
-		print("and the male dancer is : " + male.name);
+function dispArray(nums){
+	for(const item of nums){
+		print(item + "  ");
 	}
 }
 
 
-const males = new Queue();
-const females = new Queue();
-getDancers(males, females);
-dance(males, females);
-if (!females.empty()) {
-    print(females.front().name + " is waiting to dance.");
+const queues = [];
+ for (let i = 0; i < 10; ++i) {
+    queues[i] = new Queue();
  }
- if (!males.empty()) {
-    print(males.front().name + " is waiting to dance.");
- }
-
-
-
-
-
+ const nums = [];
+ for (let i = 0; i < 10; ++i) {
+	nums[i] = Math.floor(Math.floor(Math.random() * 101)); 
+}
+ print("Before radix sort: ");
+ dispArray(nums);
+ distribute(nums, queues, 10, 1);
+ collect(queues, nums);
+ distribute(nums, queues, 10, 10);
+ collect(queues, nums);
+ print("\n\nAfter radix sort: ");
+ dispArray(nums);
 
 
