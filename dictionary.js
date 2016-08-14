2bb6a28345113dd3b085b49363a8a0414f317d40
laibao <369632567@qq.com>
2016-08-14 12:25:50 +0800

    add a contact.txt to save all the persons names and phone numbers , complete the contacts program, test it ,completely right

--- dictionary.js
+++ dictionary.js
@@ -5,7 +5,7 @@ function Dictionary() {
 	this.remove = remove;
 	this.showAll = showAll;
 	this.count = count;
-	this.clear=clear;
+	this.clear = clear;
 }
 
 function print(item, ...values) {
@@ -14,6 +14,7 @@ function print(item, ...values) {
 
 function add(key, value) {
 	this.datastore[key] = value;
+	// this.datastore.push(key,value);
 }
 
 function find(key) {
@@ -26,34 +27,69 @@ function remove(key) {
 }
 
 function showAll() {
-	print(this.datastore)
-	for (let item in this.datastore) {
-		print(item + " -> " + this.datastore[item]);
+	for (const key of Object.keys(this.datastore).sort()) {
+		print(key + " -> " + this.datastore[key]);
 	}
 }
 
 
 function count() {
-	var n = 0;
-	for (var key in Object.keys(this.datastore)) {
-		++n;
+	let n = 0;
+	for (const key in this.datastore) {
+		n++;
 	}
 	return n;
 }
 
 
 
-
 function clear() {
-	for each(var key in Object.keys(this.datastore)) {
-		delete this.datastore[key];
-	}
+	delete this.datastore;
+	this.datastore = new Array();
 }
 
-var pbook = new Dictionary();
-pbook.add("Mike", "123");
+
+
+const pbook = new Dictionary();
+pbook.add("Raymond", "123");
 pbook.add("David", "345");
 pbook.add("Cynthia", "456");
+pbook.add("Mike", "723");
+pbook.add("Jennifer", "987");
+pbook.add("Danny", "012");
+pbook.add("Jonathan", "666");
 print("David's extension: " + pbook.find("David"));
-pbook.remove("David");
+print(pbook);
+// pbook.remove("David");
 pbook.showAll();
+print(pbook.count());
+// pbook.showAll();
+
+const str = "laowang";
+let i = 0;
+while (i < 10) {
+	document.write("<p>" + str + i + " : " + "000000" + i + "</p>");
+	i++;
+}
+
+const contacts = new Dictionary();
+
+
+contacts.read = function(file) {
+	const readContacts = read(file);
+	for (const [index, key] of readContacts.entries()) {
+		readContacts[index] = key.split(":");
+		contacts.add(readContacts[index][0].trim(), readContacts[index][1].trim());
+	}
+
+}
+
+contacts.showSingle=function(str){
+	contacts.find(str);
+	print("find the item in contacts: ");
+	print(str +" -> "+ contacts.datastore[str]);
+}
+contacts.read("contacts.txt");
+contacts.showSingle("laowang7");
+
+contacts.showAll();
