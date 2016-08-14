5ff215ec0cd3f74261f2749ca1a6a6ecb9be7f63
laibao <369632567@qq.com>
2016-08-14 15:08:27 +0800

    create hash.js, to make a hash list, get it tested

---
+++ hash.js
@@ -0,0 +1,92 @@
+function HashTable() {
+	this.table = new Array(137);
+	this.simpleHash = simpleHash;
+	this.showDistro = showDistro;
+	this.put = put;
+	//this.get = get;
+}
+
+function put(data) {
+	const pos = this.simpleHash(data);
+	this.table[pos] = data;
+}
+
+function simpleHash(string) {
+	const H = 31;
+	let total = 0;
+	for (let i = 0; i < string.length; ++i) {
+		total += H * total + string.charCodeAt(i);
+	}
+	total = total % this.table.length;
+	if (total < 0) {
+		total += this.table.length - 1;
+	}
+	return Number.parseInt(total);
+}
+
+function print(item, ...values) {
+	console.log(item, ...values);
+}
+
+function showDistro() {
+	for (const [index, item] of this.table.entries()) {
+		if (item != undefined) {
+			print(index + ": " + item)
+		}
+	}
+}
+
+
+// const someNames = ["David", "Jennifer", "Donnie", "Raymond",
+// 	"Cynthia", "Mike", "Clayton", "Danny", "Jonathan"
+// ];
+// const hTable = new HashTable();
+// for (const item of someNames) {
+// 	hTable.put(item);
+// }
+// print(hTable)
+// hTable.showDistro();
+
+
+
+/**
+	1.to save students` score 
+*/
+
+function getRandomInt(max, min) {
+	return Math.floor(Math.random() * Math.abs(max - min + 1)) + min;
+}
+
+function genStuData(arr) {
+	const len = arr.length
+	for (let i = 0; i < len; ++i) {
+		let num = "";
+		for (let j = 1; j <= 9; ++j) {
+			num += Math.floor(Math.random() * 10);
+		}
+		num += getRandomInt(50, 100);
+		// debugger;
+		arr.push(num);
+	}
+
+}
+
+
+const numStudents = 10;
+const arrSize = 97;
+const idLen = 9;
+const students = new Array(numStudents);
+genStuData(students);
+print("Student data: \n");
+for (const item of Object.keys(students)) {
+	print(students[item].substring(0, 8) + " " +
+		students[item].substring(8));
+}
+print("\n\nData distribution: \n");
+const hTable = new HashTable();
+for (const item of Object.keys(students)) {
+
+	hTable.put(students[item]);
+}
+print(hTable)
+hTable.showDistro();
